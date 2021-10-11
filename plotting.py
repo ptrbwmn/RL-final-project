14a39425d6c83dd2f572a3ca7b5ac67f8ae80943
pibo98 <pieterbouwman98@gmail.com>
2021-10-11 19:14:35 +0200

    added general experiment functionality

--- plotting.py
+++ plotting.py
@@ -1,16 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from pathlib import Path
 
 def running_mean(vals, n=1):
     cumvals = np.array(vals).cumsum()
     return (cumvals[n:] - cumvals[:-n]) / n
 
-def plot(episode_lengths, episode_returns, name):
-    n = 50
-    # We will help you with plotting this time
-    plt.plot(running_mean(episode_lengths,n))
-    plt.title('Episode lengths ' + name)
-    plt.show()
-    plt.plot(running_mean(episode_returns,n))
-    plt.title('Episode returns ' + name)
-    plt.show()
+def SavePlot(metrics, metric_names, name, dirname, smooth=False):
+    for i, metric in enumerate(metrics):
+        x=0
+        xs = []
+        ys = []
+        stds = []
+        for coordinate in metric:
+            xs.append(x)
+            ys.append(coordinate[0])
+            stds.append(coordinate[1])
+            x+=1
+
+        lower_std = [val - std for val, std in zip(ys, stds)]
+        upper_std = [val + std for val, std in zip(ys, stds)]
+
+        if smooth:
+            raise NotImplementedError
+        else:
+            plt.plot(xs,ys,label=metric_names[i])
+            plt.fill_between(xs, lower_std, upper_std, alpha=0.4)
+    
+    Path(dirname).mkdir(parents=True, exist_ok=True)
+    plt.legend()
+    plt.savefig(dirname + "/" + name + ".png")
