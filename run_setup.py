82939d55549b87a15ecda37b59e64acc434da9c6
FrankVerhoef <fsa.verhoef@chello.nl>
2021-10-13 22:19:20 +0200

    Stochastic version of lava environment

--- run_setup.py
+++ run_setup.py
@@ -1,6 +1,7 @@
 from environments import WindyGridworldEnv, BasicEnv2
 from env_dense import EmptyEnvDense5x5
 from env_lava_det import LavaDetEnv9x7
+from env_lava_stoch import LavaStoch80Env9x7
 from policy import EpsilonGreedyPolicy, EpsilonGreedyPolicy_Double_Q
 from q_learning import sarsa, q_learning, double_q_learning
 import gym
@@ -21,6 +22,8 @@ def run_setup(config, q_learning_variant):
         env = gym.make('MiniGrid-EmptyDense-5x5-v0')
     elif env == "LavaDetEnv9x7":
         env = gym.make('MiniGrid-LavaDet-9x7-v0')
+    elif env == "LavaStoch80Env9x7":
+        env = gym.make('MiniGrid-LavaStoch80-9x7-v0')
     else:
         raise NotImplementedError
     
